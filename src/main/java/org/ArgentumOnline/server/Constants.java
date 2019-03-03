9bc06c5492b26fa3e40ccad7a6c46269fb07922c
gorlok <pablofer72@yahoo.com.ar>
2019-03-03 20:26:28 -0300

    updated headers with license

--- src/main/java/org/ArgentumOnline/server/Constants.java
+++ src/main/java/org/ArgentumOnline/server/Constants.java
@@ -1,28 +1,20 @@
-/**
- * Constants.java
+/*******************************************************************************
+ *     Gorlok AO, an implementation of Argentum Online using Java.
+ *     Copyright (C) 2019 Pablo Fernando Lillia «gorlok» 
  *
- * Created on 14 de septiembre de 2003, 21:04
- * 
-    AOJava Server
-    Copyright (C) 2003-2007 Pablo Fernando Lillia (alias Gorlok)
-    Web site: http://www.aojava.com.ar
-    
-    This file is part of AOJava.
-
-    AOJava is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    AOJava is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Foobar; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA 
- */
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as
+ *     published by the Free Software Foundation, either version 3 of the
+ *     License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *******************************************************************************/
 package org.ArgentumOnline.server;
 
 /**
--- src/main/java/org/ArgentumOnline/server/util/IniFile.java
+++ src/main/java/org/ArgentumOnline/server/util/IniFile.java
@@ -1,30 +1,20 @@
-/**
- * IniFile.java
+/*******************************************************************************
+ *     Gorlok AO, an implementation of Argentum Online using Java.
+ *     Copyright (C) 2019 Pablo Fernando Lillia «gorlok» 
  *
- * Created on 17 de septiembre de 2003, by Pablo Lillia.
- * 
-    AOJava Server
-    Copyright (C) 2003-2007 Pablo Fernando Lillia (alias Gorlok)
-    Web site: http://www.aojava.com.ar
-    
-    This file is part of AOJava.
-
-    AOJava is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    AOJava is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Foobar; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-    
- */
-
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as
+ *     published by the Free Software Foundation, either version 3 of the
+ *     License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *******************************************************************************/
 package org.ArgentumOnline.server.util;
 
 import java.io.BufferedReader;
@@ -39,27 +29,7 @@ import java.util.HashMap;
 import java.util.TreeSet;
 
 /**
- * IniFile: Clase para leer archivos INI. Nota: ignora cualquier comentario
- * existente en el archivo.
- *
- * Copyright 2003 Pablo F. Lillia
- *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place, Suite 330, Boston, MA 02111-1307 USA
- *
- * You can contact me at: pablofer72 (at) yahoo (dot) com (dot) ar
- *
- * @author Pablo F. Lillia
+ * @author gorlok
  */
 public class IniFile {
 
