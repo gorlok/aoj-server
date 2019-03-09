810065b23576924f108a36e3594dad21fcd8c5a5
gorlok <pablofer72@yahoo.com.ar>
2019-03-09 19:36:23 -0300

    bug fixed with enums

--- src/main/java/org/ArgentumOnline/server/user/SpellTarget.java
+++ src/main/java/org/ArgentumOnline/server/user/SpellTarget.java
@@ -1,19 +1,20 @@
 package org.ArgentumOnline.server.user;
 
 public enum SpellTarget {
+	NONE,
+	
     /*1*/ User,
     /*2*/ Npc,
     /*3*/ UserAndNpc,
     /*4*/ Terrain;
 	
 	public byte value() { 
-		return (byte) (this.ordinal() + 1);
+		return (byte) (this.ordinal());
 	}
 	
 	private final static SpellTarget[] VALUES = SpellTarget.values();
-	
 	public static SpellTarget value(int value) {
-		return VALUES[value -1 ];
+		return VALUES[value];
 	}
 	
 }
